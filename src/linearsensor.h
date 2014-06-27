a6b504dd4766f48a35b0ff1e5f527731d5cf5556
Lucas <lucas@takejame.com.br>
2014-06-27 16:59:25 -0300

    change: Addressing class is now Actuators burden, not its subclasses

--- src/linearsensor.h
+++ src/linearsensor.h
@@ -8,8 +8,6 @@ class LinearSensor: public Actuator{
 public:
 	int 			minimum = 0;
 	int 			maximum = 1023;
-	char 			addressing_id;
-	Addressing* 	addressing;
 
 	LinearSensor(char* name, char id): Actuator(name, id, 1 /*slots*/, 1 /*mode counter*/, 3, VISUAL_NONE){
 		Mode *mode = supports("linear");
@@ -34,31 +32,31 @@ public:
 		
 		float scaleMin, scaleMax;
 
-		scaleMin = this->addressing->minimum.f;
-	    scaleMax = this->addressing->maximum.f;
+		scaleMin = this->addrs[0]->minimum.f;
+	    scaleMax = this->addrs[0]->maximum.f;
 
 		// Convert the sensor scale to the parameter scale
-	    if (this->addressing->port_properties & PROPERTY_LOGARITHM) {
+	    if (this->addrs[0]->port_properties & PROPERTY_LOGARITHM) {
 	    	scaleMin = log(scaleMin)/log(2);
 	    	scaleMax = log(scaleMax)/log(2);
 	    } 
 
 	    // Parameter is linear
-		this->value = (sensor - this->minimum) * (this->addressing->maximum.f - this->addressing->minimum.f);
+		this->value = (sensor - this->minimum) * (this->addrs[0]->maximum.f - this->addrs[0]->minimum.f);
 	    this->value /= (this->maximum - this->minimum);
-	    this->value += this->addressing->minimum.f;
+	    this->value += this->addrs[0]->minimum.f;
 	    
-	    if (this->addressing->port_properties & PROPERTY_LOGARITHM) {
+	    if (this->addrs[0]->port_properties & PROPERTY_LOGARITHM) {
 	    	this->value = pow(2,this->value);
 	    }
 
-	    if (this->addressing->port_properties & PROPERTY_INTEGER) {
+	    if (this->addrs[0]->port_properties & PROPERTY_INTEGER) {
 	    	this->value = floor(this->value);
 	    }
  	}
 
 	void getUpdates(Update* update){
-	    update->updates->setup(this->addressing_id, this->value);
+	    update->updates->setup(this->addrs[0]->id, this->value);
 	}
 
 	void postMessageChanges(){}

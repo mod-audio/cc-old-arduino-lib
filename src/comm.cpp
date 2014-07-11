a3bf7a2643cb8ec3a276aea7eefc982eef3ef21b
Ricardo Crudo <ricardo.crudo@gmail.com>
2014-07-11 16:20:59 -0300

    Added chain communication layer

---
+++ src/comm.cpp
@@ -0,0 +1,197 @@
+
+// includes
+#include "comm.h"
+#include <Arduino.h>
+
+// defines
+enum {SYNC, DESTINATION, ORIGIN, FUNCTION, DATA_SIZE_LSB, DATA_SIZE_MSB, DATA, CHECKSUM, UNKNOWN = 0xFF};
+
+// macros
+#define READ_MODE()     digitalWrite(SERIAL_READ_WRITE_PIN, LOW)
+#define WRITE_MODE()    digitalWrite(SERIAL_READ_WRITE_PIN, HIGH); delayMicroseconds(10)
+
+// local variables
+static chain_t g_chain;
+static void (*g_parser_cb)(chain_t *chain_data) = NULL;
+static uint8_t g_address;
+
+////////////////////////////////////////////////////////////////////////////////
+// local functions definitions
+
+static uint8_t decode(uint8_t byte, uint8_t *decoded)
+{
+    static uint8_t escape;
+
+    (*decoded) = byte;
+
+    if (escape)
+    {
+        if (byte == CHAIN_ESCAPE_BYTE)
+        {
+            (*decoded) = CHAIN_ESCAPE_BYTE;
+        }
+        else if (byte == ~CHAIN_SYNC_BYTE)
+        {
+            (*decoded) = CHAIN_SYNC_BYTE;
+        }
+
+        escape = 0;
+    }
+    else if (byte == CHAIN_ESCAPE_BYTE)
+    {
+        escape = 1;
+        return 0;
+    }
+
+    return 1;
+}
+
+static uint8_t encode(uint8_t byte, uint8_t *buffer)
+{
+    if (byte == CHAIN_SYNC_BYTE)
+    {
+        buffer[0] = CHAIN_ESCAPE_BYTE;
+        buffer[1] = ~CHAIN_SYNC_BYTE;
+        return 2;
+    }
+    else if (byte == CHAIN_ESCAPE_BYTE)
+    {
+        buffer[0] = CHAIN_ESCAPE_BYTE;
+        buffer[1] = CHAIN_ESCAPE_BYTE;
+        return 2;
+    }
+    else
+    {
+        buffer[0] = byte;
+        return 1;
+    }
+
+    return 0;
+}
+
+static bool chain_fsm(uint8_t byte) 
+{
+    static uint8_t state = UNKNOWN, checksum;
+    static uint16_t received;
+    uint16_t tmp;
+
+    if (byte == CHAIN_SYNC_BYTE) state = SYNC;
+    if (state != CHECKSUM) checksum += byte;
+
+    switch (state)
+    {
+        case SYNC:
+            g_chain.data_size = 0;
+            received = 0;
+            checksum = CHAIN_SYNC_BYTE;
+            state++;
+            break;
+
+        case DESTINATION:
+            if (g_address == 0 || (g_address == byte && g_address >= CHAIN_FIRST_DEV_ADDR))
+            {
+                g_chain.destination = byte;
+                state++;    
+            }
+            else
+            {
+                state = UNKNOWN;            
+            }
+            break;
+
+        case ORIGIN:
+            g_chain.origin = byte;
+            state++;
+            break;
+
+        case FUNCTION:
+            g_chain.function = byte;
+            state++;
+            break;
+
+        case DATA_SIZE_LSB:
+            g_chain.data_size = byte;
+            state++;
+            break;
+
+        case DATA_SIZE_MSB:
+            tmp = byte;
+            tmp <<= 8;
+            g_chain.data_size |= tmp;
+            state++;
+            if (g_chain.data_size == 0) state++;
+            break;
+
+        case DATA:
+            g_chain.data[received++] = byte;
+            if (received == g_chain.data_size) state = CHECKSUM;
+            break;
+
+        case CHECKSUM:
+            state = UNKNOWN;
+            if (byte == checksum) return true;
+            break;
+    }
+
+    return false;
+}
+
+static void rx_cb(uint8_t byte)
+{
+    if (!g_parser_cb) return;
+
+    if (decode(byte, &byte) && chain_fsm(byte))
+    {
+        g_parser_cb(&g_chain);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// global functions definitions
+
+void comm_setup(void (*parser_cb)(chain_t *chain))
+{
+    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.setRxCompleteCallback(rx_cb);
+    pinMode(SERIAL_READ_WRITE_PIN, OUTPUT);
+    READ_MODE();
+
+    g_parser_cb = parser_cb;
+    g_address = 0;
+}
+
+void comm_send(chain_t *chain)
+{
+    uint32_t i;
+    uint8_t *raw_data, checksum = 0;
+
+    chain->sync = CHAIN_SYNC_BYTE;
+    raw_data = (uint8_t *) &(chain->sync);
+
+    // calculate the checksum, 6 is the header size
+    for (i = 0; i < (chain->data_size + 6); i++)
+    {
+        checksum += *raw_data++;
+    }
+
+    // append the checksum to buffer
+    chain->data[chain->data_size] = checksum;
+
+    raw_data = (uint8_t *) &(chain->destination);
+
+    // send data
+    uint8_t buffer[2];
+    WRITE_MODE();
+    Serial.write(CHAIN_SYNC_BYTE);
+    for (i = 0; i < (chain->data_size + 6); i++)
+    {
+        Serial.write(buffer, encode(*raw_data++, buffer));
+        Serial.flush();
+    }
+    READ_MODE();
+}
+
+void comm_set_address(uint8_t address)
+{
+    g_address = address;
+}
---
+++ src/comm.h
@@ -0,0 +1,31 @@
+
+#ifndef COMM_H
+#define COMM_H
+
+// includes 
+#include <stdint.h>
+
+// serial definitions
+#define SERIAL_BAUD_RATE        500000
+#define SERIAL_READ_WRITE_PIN   2
+
+// control chain definitions
+#define CHAIN_SYNC_BYTE         0xAA
+#define CHAIN_ESCAPE_BYTE       0x1B
+#define CHAIN_BUFFER_SIZE       256
+#define CHAIN_FIRST_DEV_ADDR    0x80
+
+// data definitions
+typedef struct __attribute__((__packed__)) CHAIN_T {
+    uint8_t sync;
+    uint8_t destination, origin, function;
+    uint16_t data_size;
+    uint8_t data[CHAIN_BUFFER_SIZE];
+} chain_t;
+
+// functions prototypes
+void comm_setup(void (*parser_cb)(chain_t *chain));
+void comm_send(chain_t *chain);
+void comm_set_address(uint8_t address);
+
+#endif
